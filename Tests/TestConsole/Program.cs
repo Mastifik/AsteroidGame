867bf31d94d3831569e23fc5837c1486dba8c97e
Mastifik <mikota2102@gmail.com>
2020-04-22 17:34:41 +0300

    Пример определения свойства

--- Tests/TestConsole/Program.cs
+++ Tests/TestConsole/Program.cs
@@ -14,11 +14,11 @@ namespace TestConsole
             player1.Name = "Иванов";
             player1.Birthday = new DateTime(1974, 12, 21, 0, 0, 0);
             */
+                       
+            Player player1 = new Player("Emrty", new DateTime(1987, 12, 12));
 
             Console.Write("Введите фамилию >");
-            var surname = Console.ReadLine();
-
-            Player player1 = new Player(surname, new DateTime(1987, 12, 12));
+            player1.Name = Console.ReadLine();
 
             Console.WriteLine(player1.GetName());
 
@@ -41,21 +41,38 @@ namespace TestConsole
             _Name = Name;
         }
 
+        public string Name
+        {
+            get
+            {
+                return _Name;
+            }
+
+            set
+            {
+                _Name = value;
+            }
+        }
+
+
+        public string Surename { get; set; }
+
+
         public Player() 
         {
-
+            Surename = "123";
         }
 
         public Player(string Name) 
         {
             this.Name = Name;
-            Birthday = DateTime.Now;
+            _Birthday = DateTime.Now;
         }
 
         public Player(string Name, DateTime Birthday)
             :this(Name)
         {
-            this.Birthday = Birthday;
+            this._Birthday = Birthday;
 
         }
     }
