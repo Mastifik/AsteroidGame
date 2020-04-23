fa9dc4f496dd6475b97807359e83ee79c98af338
Mastifik <mikota2102@gmail.com>
2020-04-23 15:36:10 +0300

    Добавление звезд в игровую логику

--- AsteroidGame/Game.cs
+++ AsteroidGame/Game.cs
@@ -58,11 +58,18 @@ namespace AsteroidGame
         {
             __GameObjects = new VisualObject[30];
 
-            for (var i = 0; i < __GameObjects.Length; i++)
+            for (var i = 0; i < __GameObjects.Length / 2 ; i++)
             {
                 __GameObjects[i] = new VisualObject(new Point(600, i * 20),
                     new Point(15 - i, 20 - i), new Size(20, 20));
             }
+
+            for (var i =  __GameObjects.Length / 2; i < __GameObjects.Length; i++)
+            {
+                __GameObjects[i] = new Star(new Point(600, (int)(1 / 2.0 * 20)),
+                    new Point(- i, 0), 10);
+            }
+             
         }
 
         public static void Update()
