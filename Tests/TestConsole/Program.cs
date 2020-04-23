b8ab9144f4f91a305b97f4c79e3ae7c594978c27
Mastifik <mikota2102@gmail.com>
2020-04-23 11:04:16 +0300

    Примеры описания класса Вектор-операторы (сумма разность и приведение типов)

--- Tests/TestConsole/Program.cs
+++ Tests/TestConsole/Program.cs
@@ -15,12 +15,26 @@ namespace TestConsole
             player1.Birthday = new DateTime(1974, 12, 21, 0, 0, 0);
             */
                        
-            Player player1 = new Player("Emrty", new DateTime(1987, 12, 12));
+            /*Player player1 = new Player("Emrty", new DateTime(1987, 12, 12));
 
             Console.Write("Введите фамилию >");
             player1.Name = Console.ReadLine();
 
             Console.WriteLine(player1.GetName());
+            */
+
+
+            var v1 = new Vector2D(5, 7);
+            var v2 = new Vector2D(-7, 2);
+
+            var v3 = v1 + v2;
+
+            var v4 = v3 + 3.14159265358979;
+
+            double pi = 3.1515;
+            int i = (int)pi;
+
+            double Length = v4;
 
             Console.ReadLine();
         }
