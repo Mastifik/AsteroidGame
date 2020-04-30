55c50226ba37d088d0d11837808fd9261c8ef994
Mastifik <mikota2102@gmail.com>
2020-04-30 14:33:49 +0300

    Комбинированный логгер

--- Tests/TestConsole/Program.cs
+++ Tests/TestConsole/Program.cs
@@ -12,12 +12,17 @@ namespace TestConsole
             //Logger log = new TextFileLogger("text.log");
             //Logger log = new ConsoleLogger();
             //Logger log = new DebugOutputLogger();
-            Logger log = new TraceLogger();
+            //Logger log = new TraceLogger();
 
 
             Trace.Listeners.Add(new TextWriterTraceListener("logger.log"));
             Trace.Listeners.Add(new XmlWriterTraceListener("logger.log.xml"));
 
+            CombineLogger log = new CombineLogger();
+            log.Add(new CombineLogger());
+            log.Add(new DebugOutputLogger());
+            log.Add(new TraceLogger());
+            log.Add(new TextFileLogger("new_log.log"));
 
             log.LogInformation("Messge1");
             log.LogWarning("Info message");
@@ -25,7 +30,7 @@ namespace TestConsole
 
             log.Flush();
 
-            //Console.ReadLine();
+            Console.ReadLine();
             
         }       
     }            
