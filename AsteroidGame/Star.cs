0d44d63fee184d65bb9530725aae9a33efe83465
Mastifik <mikota2102@gmail.com>
2020-04-30 15:24:50 +0300

    Рефакторинг процесса загрузки игровых объектов

--- AsteroidGame/Star.cs
+++ AsteroidGame/Star.cs
@@ -7,32 +7,46 @@ using System.Threading.Tasks;
 
 namespace AsteroidGame
 {
-    class Star : VisualObject
+    class Star
     {
-        public Star(Point Position, Point Direction, int Size) : base(Position, Direction, new Size(Size, Size))
-        {
+        private Point point1;
+        private Point point2;
+        private int v;
 
+        public Star(Point point1, Point point2, int v)
+        {
+            this.point1 = point1;
+            this.point2 = point2;
+            this.v = v;
         }
 
-        public override void Draw(Graphics g)
+        private class Star : VisualObject
         {
-            g.DrawLine(Pens.WhiteSmoke,
-                _Position.X, _Position.Y,
-                _Position.X + _Size.Width,
-                _Position.Y + _Size.Height);
+            public Star(Point Position, Point Direction, int Size) : base(Position, Direction, new Size(Size, Size))
+            {
 
-            g.DrawLine(Pens.White,
-                _Position.X + _Size.Width,
-                _Position.Y, _Position.X,
-                _Position.Y + _Size.Height);
-        }
+            }
+
+            public override void Draw(Graphics g)
+            {
+                g.DrawLine(Pens.WhiteSmoke,
+                    _Position.X, _Position.Y,
+                    _Position.X + _Size.Width,
+                    _Position.Y + _Size.Height);
+
+                g.DrawLine(Pens.White,
+                    _Position.X + _Size.Width,
+                    _Position.Y, _Position.X,
+                    _Position.Y + _Size.Height);
+            }
 
 
-        public override void Update()
-        {
-            _Position.X += _Direction.X;
-            if (_Position.X < 0)
-                _Position.X = Game.Width + _Size.Width;
+            public override void Update()
+            {
+                _Position.X += _Direction.X;
+                if (_Position.X < 0)
+                    _Position.X = Game.Width + _Size.Width;
+            }
         }
     }
 }
