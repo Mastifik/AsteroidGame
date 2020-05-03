72f7755fc50868330c4c2872dd0ce711f657b55d
Mastifik <mikota2102@gmail.com>
2020-05-03 19:13:45 +0300

    Пример работы с исключениями

--- Tests/TestConsole/Program.cs
+++ Tests/TestConsole/Program.cs
@@ -2,6 +2,7 @@
 using System.Globalization;
 using TestConsole.Loggers;
 using System.Diagnostics;
+using System.Runtime.Serialization;
 
 namespace TestConsole
 {
@@ -32,7 +33,7 @@ namespace TestConsole
             Student student = new Student { Name = "Иванов" };
 
             ILogger log = combine_log;
-            ComputerLongDataValue(100, student);
+           /* ComputerLongDataValue(100, student);
 
             Console.WriteLine("Программа завершена!");
             Console.ReadLine();
@@ -40,7 +41,35 @@ namespace TestConsole
             using (var file_logger = new TextFileLogger("another.log"))
             {
                 file_logger.LogInformation("123");
+            }*/
+
+            try
+            {
+                 ComputerLongDataValue(600, log);
             }
+
+            catch (ArgumentNullException error)
+            {
+                combine_log.LogError(error.ToString());
+                combine_log.LogError(error.Message);
+                throw new ComputeExceptionException("Ошибка в значении входного параметра", error);
+            }
+
+            catch (InvalidOperationException)
+            {
+                Console.WriteLine("Число итераций слишком велико!");
+                throw;
+            }
+
+            catch (Exception error)
+            {
+                combine_log.LogError(error.ToString());
+                combine_log.LogError(error.Message);
+                throw new ComputeExceptionException("Произошла неизвестная ошибка при вычислении", error);
+
+            }
+
+
 
             combine_log.Flush();
 
@@ -49,15 +78,41 @@ namespace TestConsole
 
         private static double ComputerLongDataValue(int Count, ILogger Log)
         {
+            if (Log is null)
+                throw new ArgumentNullException(nameof(Log));
+
+            if (Count <= 0)
+                throw new ArgumentOutOfRangeException(nameof(Count), Count, "Число итераций обязано быть больше нуля!");
+
             var result = 0;
             for (var i = 0; i < Count; i++)
             {
                 result++;
                 Log.Log($"Вычисление итерации{i}");
-                System.Threading.Thread.Sleep(100);
+                System.Threading.Thread.Sleep(10);
+
+                if (i > 500)
+                    throw new InvalidOperationException("Число итерации оказалось слишком большим",
+                        new ArgumentException("Число итерации было указано больше 500 и равно {Count}", nameof(Count)));
             }
 
             return result;
         }
-    }            
+    }
+
+   [Serializable]
+
+   public class ComputeExceptionException : ApplicationException
+    {
+        public ComputeExceptionException() { }
+        public ComputeExceptionException(string message) : base(message) { }
+        public ComputeExceptionException(string message, Exception inner) : base(message, inner) { }
+
+
+        protected ComputeExceptionException(SerializationInfo info, StreamingContext context) : base(info, context)
+        {
+
+        }
+    }
+
 }
