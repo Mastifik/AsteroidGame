ab47a96fb0c97fd523f6e3686097772696d6db1e
Mastifik <mikota2102@gmail.com>
2020-05-06 19:24:04 +0300

    Пример использования делегатов для обработки данных в некотором перечеслении

--- Tests/TestConsole/Program.cs
+++ Tests/TestConsole/Program.cs
@@ -4,9 +4,14 @@ using TestConsole.Loggers;
 using System.Diagnostics;
 using System.Runtime.Serialization;
 using TestConsole.Service;
+using System.Collections.Generic;
 
 namespace TestConsole
 {
+    internal delegate int StringProcessor(string str);
+
+    internal delegate void StudentProcessor(Student student);
+
     class Program
     {
         static void Main(string[] args)
@@ -33,7 +38,7 @@ namespace TestConsole
 
             decanat.Remove(student_to_remove);
 
-            var random_student = new Student { Name = rnd.GetValue<string>("Иванов", "Петров", "Сидоров") };
+            var random_student = new Student { SurName = rnd.GetValue<string>("Иванов", "Петров", "Сидоров") };
 
             var random_rating = rnd.GetValue<int>(2, 3, 4, 5);
 
@@ -42,12 +47,57 @@ namespace TestConsole
             var decanat2 = new Decanat();
             decanat2.LoadFromFile("decanat.csv");
 
+            StringProcessor str_rocessor = new StringProcessor(GetStringLenght);
+
+            var lenght = str_rocessor("Hello World");
+
+            /*StudentProcessor process = new StudentProcessor(PrintStudent);
+
+            process(random_student);
+            
+            process = RateStudent;
+            
+            process(random_student);
+            
+            process = PrintStudent;
+            
+            process(random_student);*/
+
+            //ProcessStudents(decanat2, PrintStudent);
+            ProcessStudents(decanat2, RateStudent);
+            ProcessStudents(decanat2, PrintStudent);
+
+            var decanat3 = new Decanat();
+
+            ProcessStudents(decanat2, decanat3.Add);
+
             Console.ReadLine();
 
         }
 
+        private static int GetStringLenght(string str)
+        {
+            return str.Length;
+        }
+
+        private static void PrintStudent(Student student)
+        {
+            Console.WriteLine("[{0}]{1} {2} {3} - {4}",
+                student.Id, student.SurName, student.Name, student.Patronimyc, student.AverageRating);
+        }
+
+        private static void RateStudent(Student student)
+        {
+            var rnd = new Random();
+            student.Ratings.AddRange(rnd.GetValues(5, 2, 6));
+        }
 
 
+        private static void ProcessStudents(IEnumerable<Student> students, StudentProcessor Processor)
+        {
+            foreach (var student in students)
+                Processor(student);
+        }
     }
 
 
