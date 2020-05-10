87b8fc77a3c3213c95b19275347a45e1eabd1e69
Mastifik <mikota2102@gmail.com>
2020-05-10 15:16:17 +0300

    Урок4.Задание2а

---
+++ Урок4,Задание 2а.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+public class Class1
+{
+	public Class1()
+	{
+		var numbers = new List<int>() { 1, 2, 2, 1, 2, 3 };
+		numbers.Sort();
+
+		var result = new Dictionary<int, int>();
+		for (int i = 1; i < numbers.Count; i++)
+		{
+			if (numbers[i] == numbers[i - 1])
+			{
+				if (!result.Keys.Contains(numbers[i]))
+					result.Add(numbers[i], 1);
+				else result[numbers[i]]++;
+
+			}
+		}
+		foreach (var x in result.Keys)
+			if (result[x] > 1) Console.WriteLine("Число {0} встречается {1} раз.", x, result[x]);
+	}
+}
