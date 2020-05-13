cafd1544d2f7304d7391074492b7d67e735c8207
Mastifik <mikota2102@gmail.com>
2020-05-14 00:23:49 +0300

    Стили...

--- SumplesUI/TextEditorWPF/MainWindow.xaml.cs
+++ SumplesUI/TextEditorWPF/MainWindow.xaml.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using Microsoft.Win32;
+using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -28,9 +30,39 @@ namespace TextEditorWPF
             //Height = 600;
         }
 
-        private void OnCloseMenuItemClick(object sender, RoutedEventArgs e)
+        private void OnCloseFileMenuClick(object sender, RoutedEventArgs e)
         {
             Close();
+        }
+
+        private void OnOpenFileMenuClick(object sender, RoutedEventArgs e)
+        {
+            var dialog = new OpenFileDialog
+            {
+                Title = "Выбор файла для редактирования",
+                Filter = "Текстовые файлы (*.txt)|*.txt|Все файлы (*.*)|*.*"
+            };
+
+            if (dialog.ShowDialog() != true) return;
+
+            var file_name = dialog.FileName;
+
+            MainTextEdit.Text = FileDialog.ReadAllText(file_name);
+        }
+
+        private void OnSaveFileMenuClick(object sender, RoutedEventArgs e)
+        {
+            var dialog = new SaveFileDialog
+            {
+                Title = "Выбор файла для редактирования",
+                Filter = "Текстовые файлы (*.txt)|*.txt|Все файлы (*.*)|*.*"
+            };
+            
+            if(dialog.ShowDialog() != true) return;
+
+            var file_name = dialog.FileName;
+
+            File.WriteAllText(file_name, MainTextEdit.Text);
         }
     }
 }
