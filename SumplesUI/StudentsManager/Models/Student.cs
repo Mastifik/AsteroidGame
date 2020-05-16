62388dc0b156e9708af12bbf5e0135c94071e626
Mastifik <mikota2102@gmail.com>
2020-05-16 18:45:25 +0300

    Обеспечение возможности информирования о том, что свойство Name  класса Student изменилось

--- SumplesUI/StudentsManager/Models/Student.cs
+++ SumplesUI/StudentsManager/Models/Student.cs
@@ -1,16 +1,30 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace StudentsManager.Models
 {
-    internal class Student
+    internal class Student : INotifyPropertyChanged
     {
+        public event PropertyChangedEventHandler PropertyChanged;
+
         public int Id { get; set; }
 
-        public string Name { get; set; }
+        private string _Name;
+        public string Name 
+        {
+            get => _Name;
+            set
+            {
+                if (_Name == value) return;
+                _Name = value;
+                PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(Name)));
+
+            }
+        }
 
         public string Surname { get; set; }
 
