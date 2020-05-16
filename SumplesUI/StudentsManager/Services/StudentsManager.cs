63c6e5cd030bbf3378b91439ecbd10c52b028f57
Mastifik <mikota2102@gmail.com>
2020-05-16 14:18:44 +0300

    Менеджер студентов  - предметная область

--- SumplesUI/StudentsManager/Services/StudentsManager.cs
+++ SumplesUI/StudentsManager/Services/StudentsManager.cs
@@ -3,11 +3,39 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using StudentsManager.Models;
 
 namespace StudentsManager.Services
 {
      internal class StudentsManager
     {
+        public List<StudentsGroup> Groups { get; private set; }
 
+        public StudentsManager()
+        {
+            var student_id = 1;
+            Groups = Enumerable
+                .Range(1, 10)
+                .Select(i => new StudentsGroup
+                {
+                    Id = i, Students = Enumerable.Range(1, 10)
+                    .Select(j => new Student
+                    {
+                        Id = student_id,
+                        Name = $"Name {student_id}",
+                        Surname = $"Surname {student_id}",
+                        Patronymic = $"Patronymic {student_id++}"
+                    })
+                    .ToList()
+                })  
+                .ToList();
+
+            foreach(var group in Groups)
+            foreach (var student in group.Students)
+                student.Group = group;
+            
+
+            
+        }
     }
 }
