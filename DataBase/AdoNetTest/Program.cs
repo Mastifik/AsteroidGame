68ae64ad8d89a69fdbf274b11cd17dde4145b707
Mastifik <mikota2102@gmail.com>
2020-05-20 21:26:07 +0300

    Мзвлечение информации о строке подключения к БД файла конфигурации

--- DataBase/AdoNetTest/Program.cs
+++ DataBase/AdoNetTest/Program.cs
@@ -5,6 +5,7 @@ using System.Data.SqlClient;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Configuration;
 
 namespace AdoNetTest
 {
@@ -12,18 +13,24 @@ namespace AdoNetTest
     {
         static void Main(string[] args)
         {
-            var connection_string = @"Data Source=(localdb)\MSSQLLocalDB;Initial Catalog=AsteroidsDB-test;Integrated Security=True";
+            /* var connection_string = @"Data Source=(localdb)\MSSQLLocalDB;Initial Catalog=AsteroidsDB-test;Integrated Security=True";
 
-            //DbConnectionStringBuilder builder = new DbConnectionStringBuilder();
-            //var new_connectionstring = builder.ConnectionString;
+             //DbConnectionStringBuilder builder = new DbConnectionStringBuilder();
+             //var new_connectionstring = builder.ConnectionString;
 
-            var sql_server_connectio_string_builder = new SqlConnectionStringBuilder(connection_string);
-            sql_server_connectio_string_builder.IntegratedSecurity = false;
-            sql_server_connectio_string_builder.UserID = "User";
-            sql_server_connectio_string_builder.Password = "Password";
-            sql_server_connectio_string_builder.InitialCatalog = "TestDB123";
+             var sql_server_connectio_string_builder = new SqlConnectionStringBuilder(connection_string);
+             sql_server_connectio_string_builder.IntegratedSecurity = false;
+             sql_server_connectio_string_builder.UserID = "User";
+             sql_server_connectio_string_builder.Password = "Password";
+             sql_server_connectio_string_builder.InitialCatalog = "TestDB123";
 
-            var new_connection_string = sql_server_connectio_string_builder.ConnectionString;
+             var new_connection_string = sql_server_connectio_string_builder.ConnectionString;*/
+
+            const string connection_string_name = "DefaultConnection";
+
+            var connection_string = ConfigurationManager.ConnectionStrings[connection_string_name].ConnectionString;
+
+            Console.ReadLine();
         }
     }
 }
