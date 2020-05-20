42d9bcbb8600a8163977ded4abd0d9858a2026c4
Mastifik <mikota2102@gmail.com>
2020-05-20 21:04:06 +0300

    Строки подключения в БД

---
+++ DataBase/AdoNetTest/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Common;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdoNetTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var connection_string = @"Data Source=(localdb)\MSSQLLocalDB;Initial Catalog=AsteroidsDB-test;Integrated Security=True";
+
+            //DbConnectionStringBuilder builder = new DbConnectionStringBuilder();
+            //var new_connectionstring = builder.ConnectionString;
+
+            var sql_server_connectio_string_builder = new SqlConnectionStringBuilder(connection_string);
+            sql_server_connectio_string_builder.IntegratedSecurity = false;
+            sql_server_connectio_string_builder.UserID = "User";
+            sql_server_connectio_string_builder.Password = "Password";
+            sql_server_connectio_string_builder.InitialCatalog = "TestDB123";
+
+            var new_connection_string = sql_server_connectio_string_builder.ConnectionString;
+        }
+    }
+}
