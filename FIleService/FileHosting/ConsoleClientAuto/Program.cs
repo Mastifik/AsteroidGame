9d11be0c6fb6e8ce1b1d94b9a4712c66bfde15de
Mastifik <mikota2102@gmail.com>
2020-05-23 16:36:50 +0300

    Автоматический генерируемый клиент

---
+++ FIleService/FileHosting/ConsoleClientAuto/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleClientAuto
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var client = new FileServiceClient();
+
+            var files = client.GetFiles("c:\\123");
+
+            foreach (var File in files)
+            {
+                Console.WriteLine(file.FullName);
+            }
+            client.StartProccer("calc", "");
+
+            Console.ReadLine();
+        }
+    }
+}
